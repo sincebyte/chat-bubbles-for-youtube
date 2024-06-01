ba3ad65f9ef7532b918c1a3810ebdf9cc3206885
liu.zijie <at18708169062@gmail.com>
2024-06-01 17:39:18 +0800

    feat: auto key

--- keyboard/go.mod
+++ keyboard/go.mod
@@ -4,5 +4,6 @@ go 1.18
 
 require (
 	github.com/GuoFlight/gkeybd v0.0.1 // indirect
-	github.com/micmonay/keybd_event v1.1.1 // indirect
+	github.com/micmonay/keybd_event v1.1.2 // indirect
+	github.com/sincebyte/gkeybd v0.1.1 // indirect
 )
--- keyboard/main.go
+++ keyboard/main.go
@@ -1,21 +1,33 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
 	"time"
 
 	"github.com/sincebyte/gkeybd"
 )
 
 func main() {
-	gkeybd.TypeStr("hello world!\n")
-	time.Sleep(3 * time.Second)
-	gkeybd.TypeStr("It looks like there is an issue \n" +
-		"with your code file, `main.go`. \n" +
-		"The error message indicates that \n" +
-		"the first line of the file should \n" +
-		"contain a 'package' keyword, but \n" +
-		"instead, it has a 'EOF' (End Of \n" +
-		"File) marker.\n")
-	time.Sleep(3 * time.Second)
-	gkeybd.TypeStr("123 world!\n")
+	file := os.Args[1:]
+
+	time.Sleep(5 * time.Second)
+	data, err := ioutil.ReadFile(file[0])
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	content := string(data)
+	lines := strings.Split(content, "\n") // 按照换行符分割字符串
+
+	for _, line := range lines { // 遍历切片，并打印每一行
+		gkeybd.TypeStr(line)
+		gkeybd.TypeStr("\n")
+		line = strings.TrimSpace(line)
+		if len(line) == 0 || line[0] == '#' {
+			time.Sleep(3 * time.Second)
+		}
+	}
 }
